9a8fbf89e154d9949480cbf9ea89cec1e8d05f85
Adrian Th <adrian.theo@gmail.com>
2016-09-24 20:49:03 -0500

    Added typings

---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/express-serve-static-core/index.d.ts" />
+/// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/serve-static/index.d.ts" />
