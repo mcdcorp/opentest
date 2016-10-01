5eff78a70824e390f0c0a2de46ea932c68f01583
Adrian Th <adrian.theo@gmail.com>
2016-10-01 11:22:45 -0500

    Converted to TypeScript

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/express-serve-static-core/index.d.ts" />
-/// <reference path="globals/express/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/serve-static/index.d.ts" />
